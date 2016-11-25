82772a021dfc32bad7c1bf9a55f03f1367a5d3a0
Benny <hoaben13@student.westerdals.no>
2016-11-25 14:41:49 +0100

    starting with the basics.

---
+++ WhatsUp/WhatsUp/Controllers/CustomerController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Xml.Linq;
+
+namespace WhatsUp.Controllers
+{
+    public class CustomerController : ApiController
+    {
+        public HttpResponseMessage GetAllArticles()
+        {
+            XElement articlesXML = XElement.Load(GetFilePath());
+
+            var articleList = from articles in articlesXML.Descendants("article")
+                           select articles;
+            return Request.CreateResponse(HttpStatusCode.OK, articleList);
+        }
+
+        private string GetFilePath()
+        {
+            return System.Web.Hosting.HostingEnvironment.MapPath(@"~/App_Data/articleDB.xml");
+        }
+    }
+}
